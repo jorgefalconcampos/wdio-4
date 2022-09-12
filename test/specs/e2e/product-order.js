dff3d73d061fe2bbd6506ea145b6f202be8939cb
Jorge Falcón <jorgef_campos@outlook.com>
2022-09-12 17:42:22 -0500

    product order

--- test/specs/e2e/product-order.js
+++ test/specs/e2e/product-order.js
@@ -1,13 +1,14 @@
 describe("Successfully request for a product", () => {
-    before(async () => {
+  before(async () => {
+    await browser.url("https://www.saucedemo.com/");
+    await browser.sauceLogin();
+  });
 
-    });
+  after(async () => {
+    await browser.sauceLogout();
+  });
 
-    after(async () => {
-
-    });
-
-    it.describe("Should complete product order", async () => {
-        
-    })
-})
+  it.describe("Should complete product order", async () => {
+    await browser.pause(3000);
+  });
+});
